6c65ac57edd063e64009bcfb3fb0730087a306ba
dsf <1455025448@qq.com>
2020-01-19 07:13:54 +0800

    线段树ing

---
+++ src/main/java/dsf/algrithm/archive/data_staruct/line_tree/P732.java
@@ -0,0 +1,26 @@
+package dsf.algrithm.archive.data_staruct.line_tree;
+
+/**
+ * https://leetcode-cn.com/problems/my-calendar-iii/
+ * 线段树
+ * 线段树解法不会做。。边界太大。。。1e9!!!!
+ *
+ * @author 董少飞
+ * @date 2020/1/19
+ */
+public class P732 {
+
+    class MyCalendarThree {
+
+        int[] tree, arr =
+
+        public MyCalendarThree() {
+
+
+        }
+
+        public int book(int start, int end) {
+
+        }
+    }
+}
